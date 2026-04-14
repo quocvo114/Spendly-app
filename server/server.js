d916537b308b91856bc60e3582a1be68952a2118
quocvo114 <quocvovan123@gmail.com>
2026-04-14 21:25:07 +0700

    Configure production environment URLs

--- server/server.js
+++ server/server.js
@@ -15,7 +15,12 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "*",
+  origin: [
+    process.env.CLIENT_URL_PRODUCTION,
+    process.env.CLIENT_URL_LOCAL,
+    "http://localhost:5173",
+    "http://localhost:3000"
+  ],
   credentials: true
 }));
 
